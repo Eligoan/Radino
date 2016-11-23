b15cdd6c4a17d804c99a4a646907316a470e116f
BeanOfChaos <benjarte@ulb.ac.be>
2016-11-23 15:32:50 +0100

    Update Radino.py

--- Radino.py
+++ Radino.py
@@ -1,34 +1,32 @@
 class Graphe(dict):
-
 	def __init__(self,fichier):
 		dict.__init__(self)
-		self.fichier=fichier
+		self.fichier = fichier
+		parsing(self)
 
 	def parsing(self):
 		"""
 		Parsing the input file into a dict of dict.
 		"""
-		line=fichier.readline()
-		self.node_number=int(line)
-		line=fichier.readline()
+		inFile = open(self.fichier, "r")
+		line = inFile.readline()
+		self.node_number = int(line)
+		line = inFile.readline()
 		while line:
-			info=line.split()
+			info = line.split()
 			if info[0] not in self:#New person
-				self[info[0]]={}
+				self[info[0]] = {}
 			if info[1] not in self[info[0]]:#Multiple debts
-				self[info[0]][info[1]]=0
-			self[info[0]][info[1]]+=int(info[2])
-			line=fichier.readline()
+				self[info[0]][info[1]] = 0
+			self[info[0]][info[1]] += int(info[2])
+			line = inFile.readline()
 
 	def simplify_cycle(self,name_list):
 		"""
-		Simplify the debts between two people.
+		Simplify the debts in a cycle of people.
 		"""
-		minima = 9001 #It's over 9000
-		for i in range(len(name_list)):
-			minimum = min(self[name_list[i-1]][name_list[i]],minimum)
-		for i in range(len(name_list)):
-			self[name_list[i-1]][name_list[i]]-=minimum
+		if name_list[0] == name_list[-1]:
+			substract(name_list, searchMin(name_list)[0])
 
 	def simplify_graphe(self):
 		"""
@@ -40,15 +38,50 @@ class Graphe(dict):
 				if debtor in self[creditor]:
 					self.simplify_cycle([creditor,debtor])
 
-	def detect_communities(self):
-		#TODO TEST HS
-		communities=[]
+	def searchMin(self, path):
+		"""
+		Search for the minimum debt in a path of people
+		"""
+		nbrMini = 0
+		minimum = 9001
+		for i in range(len(path) - 1):
+			if self[path[i]][path[i+1]] == minimum:
+				nbrMini += 1
+			elif self[path[i]][path[i+1]] < minimum:
+				nbrMini = 0
+				minimum = self[path[i]][path[i+1]]
+		return minimum, nbrMini
 
-	def simplify_as_fuck(self,chemin1,chemin2):
-		#TODO : Trouver celui qui a le plus de minimums et soustraire le minimum à tout ce chemin, pour l'ajouter à tout l'autre.
+	def substract(self, path, value):
+		"""
+		Substracts value from all debts between people, following path
+		"""
+		for i in range(len(path) - 1):
+			self[path[i]][path[i+1]] -= value
+
+	def simplify_as_fuck(self,path1,path2):
+		minimum1, nbrMini1 = searchMin(path1)
+		minimum2, nbrMini2 = searchMin(path2)
+		if minimum1 == minimum2:
+			if nbrMini1 > nbrMini2:
+				self.substract(path1, minimum1)
+				self.substract(path2, -minimum1)
+			else:
+				self.substract(path2, minimum1)
+				self.substract(path1, -minimum1)
+		elif minimum1 < minimum2:
+			self.substract(path1, minimum1)
+			self.substract(path2, -minimum1)
+		else:
+			self.substract(path2, minimum2)
+			self.substract(path1, -minimum2)
 
 	def biggest_friend(self):
 		#TODO
 
 	def social_hub(self,k):
 		#TODO
+		
+	def detect_communities(self):
+		#TODO TEST HS
+		communities=[]
